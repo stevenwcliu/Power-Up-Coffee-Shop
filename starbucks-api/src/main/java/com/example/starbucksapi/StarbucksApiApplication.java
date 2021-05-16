7f1bc0f46983ca66c8ac93fc839690970fc926d5
Ajit Jakasania <43461002+Ajit-Jakasania@users.noreply.github.com>
2021-05-16 13:47:07 -0700

    update application.properties for MySQL connection

---
+++ starbucks-api/src/main/java/com/example/starbucksapi/StarbucksApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.starbucksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksApiApplication.class, args);
+	}
+
+}
